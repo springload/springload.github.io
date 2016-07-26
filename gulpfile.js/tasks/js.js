3b51a35aae21d9de653bf0956d33f292efc65301
Claire Campbell <claire.campbellnz@gmail.com>
2016-07-26 18:40:16 +1200

    Add prodution JS pipeline

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -4,40 +4,40 @@ var path = require('path');
 var browserify = require('browserify');
 // var browserifyInc = require('browserify-incremental');
 var babelify = require('babelify');
-// var envify = require('loose-envify/custom');
-// var uglifyify = require('uglifyify');
-// var uglify = require('gulp-uglify');
+var envify = require('loose-envify/custom');
+var uglifyify = require('uglifyify');
+var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
-// var gutil = require('gulp-util');
+var gutil = require('gulp-util');
 var size = require('gulp-size');
-// var bundleCollapser = require('bundle-collapser/plugin');
+var bundleCollapser = require('bundle-collapser/plugin');
 var source = require('vinyl-source-stream');
 // var bs = require('browser-sync').get('main');
 
 // var browserifyInstance = config.prod ? browserify : browserifyInc;
 var browserifyTransforms = [
     babelify,
-    // [
-    //     envify({ NODE_ENV: config.prod ? 'production' : 'development' }),
-    //     { global: true },
-    // ],
+    [
+        envify({ NODE_ENV: config.prod ? 'production' : 'development' }),
+        { global: true },
+    ],
 ];
-// var browserifyPlugins = [];
+var browserifyPlugins = [];
 
-// if (config.prod) {
-//     browserifyTransforms.push([
-//         uglifyify,
-//         // See https://github.com/hughsk/uglifyify#global-transforms
-//         { global: true },
-//     ]);
+if (config.prod) {
+    browserifyTransforms.push([
+        uglifyify,
+        // See https://github.com/hughsk/uglifyify#global-transforms
+        { global: true },
+    ]);
 
-//     browserifyPlugins.push(bundleCollapser);
-// }
+    browserifyPlugins.push(bundleCollapser);
+}
 
 var bundler = browserify({
     cache: {},
     transform: browserifyTransforms,
-    // plugin: browserifyPlugins,
+    plugin: browserifyPlugins,
     packageCache: {},
     debug: !config.prod,
     fullPaths: !config.prod,
@@ -54,7 +54,7 @@ gulp.task('js', function() {
         })
         .pipe(source(config.paths.appName))
         .pipe(buffer())
-        // .pipe(config.prod ? uglify() : gutil.noop())
+        .pipe(config.prod ? uglify() : gutil.noop())
         .pipe(size({ title: config.prod ? 'JS' : 'JS (unminified)', showFiles: true, gzip: config.prod }))
         .pipe(gulp.dest(config.paths.js));
         // .pipe(bs.stream());
