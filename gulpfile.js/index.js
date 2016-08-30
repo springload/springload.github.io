c6224dc289917c05d4967a66f1bf82ae1900e472
Thibaud Colas <thibaudcolas@gmail.com>
2016-08-30 14:25:58 +0300

    Add SVG build task to the project

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,10 +1,12 @@
 require('./tasks/watch');
 require('./tasks/content');
+require('./tasks/svg');
 require('./tasks/css');
 require('./tasks/js');
 
 var gulp = require('gulp');
 
-gulp.task('build', ['css', 'js', 'content']);
+gulp.task('build:assets', [ 'css', 'js', 'svg']);
+gulp.task('build', ['build:assets', 'content']);
 
 gulp.task('default', ['build']);
---
+++ gulpfile.js/tasks/svg.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var path = require( 'path');
+var rename = require('gulp-rename');
+var size = require('gulp-size');
+var config = require('../config');
+
+gulp.task('svg', function() {
+    return gulp
+        .src( path.join( config.paths.svg, '**', '*.svg' ) )
+        .pipe(rename({prefix: 'i-'}))
+        .pipe(svgmin())
+        .pipe(svgstore({ inlineSvg: true }))
+        .pipe(rename('svg.html'))
+        .pipe(size({ title: 'SVG', gzip: config.prod }))
+        .pipe(gulp.dest( path.join( config.paths.templates, 'core', 'includes' ) ));
+});
