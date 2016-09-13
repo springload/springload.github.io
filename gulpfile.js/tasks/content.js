cc8f6c140dd8fa3483b6f348e290313b1816e8fc
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-13 15:22:19 +0300

    Clean up content task definition

--- gulpfile.js/tasks/content.js
+++ gulpfile.js/tasks/content.js
@@ -1,9 +1,8 @@
-var gulp = require('gulp');
-var fs = require('fs');
-var path = require('path');
-var shell = require('gulp-shell');
-var config = require('../config');
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+const config = require('../config');
 
 gulp.task('content', shell.task([
-    'wrangler build ' + config.paths.content + ' ' + config.paths.www + ' --force',
+    `wrangler build ${config.paths.content} ${config.paths.www} --force`,
 ]));
+
