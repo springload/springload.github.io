3e97304bafb86ad2beef3f893279632e2cac1d91
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-13 16:43:20 +0300

    Add analytics advanced tracking with autotrack

--- static_src/js/site.js
+++ static_src/js/site.js
@@ -1,3 +1,4 @@
-var utils = require('./utils');
-const other = 'you';
-console.log(`hey ${other}`);
+import 'autotrack/lib/plugins/event-tracker';
+import 'autotrack/lib/plugins/outbound-link-tracker';
+
+console.log(`Hello, world!`);
