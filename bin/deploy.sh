42f769bdb1b3182cc5cce09e6157ea0b84541412
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-22 12:51:15 +0300

    Try to speed up Travis builds

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,20 +3,21 @@
 echo -e "Deploying to GitHub Pages...\n"
 
 if "$CI"; then
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis"
 
-  cd dist
-  git init .
-  git add -f .
-  git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
-  git push -f "https://${GH_TOKEN}@github.com/springload/springload.github.io"
+    cd dist
+    git init .
+    git add -f .
+    git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
+    git push -f "https://${GH_TOKEN}@github.com/springload/springload.github.io"
 else
-  cd dist
-  git init .
-  git add -f .
-  git commit -m "Deploy locally to GitHub pages"
-  git push -f git@github.com:springload/springload.github.io.git
+    npm run dist
+    cd dist
+    git init .
+    git add -f .
+    git commit -m "Deploy locally to GitHub pages"
+    git push -f git@github.com:springload/springload.github.io.git
 fi
 
 echo -e "Finished deploy to GitHub Pages...\n"
