f858a3240971e18235d4b87b07dd52d13d204b8f
Thibaud Colas <thibaudcolas@gmail.com>
2017-05-02 18:15:46 +0300

    Ignore wrangler errors in dev mode

--- gulpfile.js/tasks/content.js
+++ gulpfile.js/tasks/content.js
@@ -2,8 +2,12 @@ const gulp = require('gulp');
 const shell = require('gulp-shell');
 const config = require('../config');
 
+const options = {
+    ignoreErrors: !config.prod,
+};
+
 gulp.task('content', ['svg'], shell.task([
     `wrangler build ${config.paths.content} ${config.paths.www} --force`,
     // `sed -i s/UA-XXXXXXX-X/${config.values.GOOGLE_ANALYTICS}/g ${config.paths.www}/*.html`,
-]));
+], options));
 
